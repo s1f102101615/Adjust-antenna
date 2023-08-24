d9bc7741233217f44606e0dc532e2359b44d282a
s1f102101615 <s1f102101615@iniad.org>
2023-08-24 20:35:30 +0900

    urlを生成し、別ページでdlすることができるようになった

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -4,8 +4,10 @@ import { Loading } from 'src/components/Loading/Loading';
 import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
 import { userAtom } from '../atoms/user';
 import styles from './index.module.css';
+import Link from 'next/link';
 
 const Home = () => {
+  
   const [user] = useAtom(userAtom);
   const [title, setTitle] = useState('');
   const [startDate, setStartDate] = useState('');
@@ -14,28 +16,33 @@ const Home = () => {
   const [location, setLocation] = useState('');
   const [startTime, setStartTime] = useState('');
   const [endTime, setEndTime] = useState('');
-  const [generatedURL, setGeneratedURL] = useState('');
+  const [url, setUrl] = useState('');
 
   const generateURL = () => {
+    // 各フォームの値を使用してiCalendarデータを生成
     const baseURL = "BEGIN:VCALENDAR\nVERSION:2.0\n";
-    const event = `BEGIN:VEVENT\nSUMMARY:${title}\nDTSTART:${startDate}T${startTime}00Z\nDTEND:${endDate}T${endTime}00Z\nDESCRIPTION:${details}\nLOCATION:${location}\nEND:VEVENT\n`;
+    const event = `BEGIN:VEVENT\nSUMMARY:${title}\nDTSTART:${startDate}T${startTime}+0900\nDTEND:${endDate}T${endTime}+0900\nDESCRIPTION:${details}\nLOCATION:${location}\nEND:VEVENT\n`;
     const endCalendar = "END:VCALENDAR";
-  
+
     const iCalendarData = baseURL + event + endCalendar;
     const encodedData = encodeURIComponent(iCalendarData);
-  
-    const link = document.createElement("a");
-    link.setAttribute("href", `data:text/calendar;charset=utf-8,${  encodedData}`);
-    link.setAttribute("download", "event.ics");
-    link.style.display = "none";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+    setUrl(`http://localhost:3000/calendar?data=${encodeURIComponent(`data:text/calendar;charset=utf-8,${encodedData}`)}`);
+    // 生成したiCalendarデータを含むURLを返す
+    return ;
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     // Handle form submission here
+  };
+
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(url);
+      console.log('URL copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy URL: ', err);
+    }
   };
 
   if (!user) return <Loading visible />;
@@ -74,9 +81,17 @@ const Home = () => {
             <input type="text" className={styles.labels} value={location} onChange={(e) => setLocation(e.target.value)} />
           </div>
           <button className={styles.submitButton} type="submit" onClick={generateURL}>URL生成</button>
+          
         </form>
-      </div>
+        <div><Link href={url} >aaa</Link></div>
+        {/* リンクを表示してワンクリックでコピー出来るようにする */}
+        <div>
+          <input type="text" value={url} className={styles.url} readOnly />
+          <button onClick={handleCopyClick}>Copy</button>
+        </div>
+        
 
+      </div>
     </>
   );
 };
