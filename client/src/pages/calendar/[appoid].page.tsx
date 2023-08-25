ecf2cc636a566ac1bcca73e57e3a31d0ef0c144c
s1f102101615 <s1f102101615@iniad.org>
2023-08-25 19:52:06 +0900

    何回やっても2回dlされる

--- client/src/pages/calendar/[appoid].page.tsx
+++ client/src/pages/calendar/[appoid].page.tsx
@@ -15,52 +15,39 @@ const Calendar = () => {
   const { appoid } = router.query;
   const [nowEvent, setNowEvent] = useState<CalendarModel>();
 
-  const fetchEvent = async () => {
-    if (appoid == null) {
-      return;
-    }
-    const response = await apiClient.calendar.get({query: { appoid: appoid as string } });
-    const nowEvent = response.body;
-    console.log(nowEvent)
-    setNowEvent(nowEvent); // update the type of nowEvent to match CalendarModel
-  }
-
   useEffect(() => {
-    fetchEvent();
-    const encodedData = nowEvent;
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    let calendarApp = '';
-    if (userAgent.indexOf('iphone') > 0) {
-      calendarApp = 'apple_calendar';
-    } else if (userAgent.indexOf('android') > 0) {
-      calendarApp = 'google_calendar';
-    } else if (userAgent.indexOf('windows') > 0) {
-      calendarApp = 'outlook_calendar';
+    const fetchEvent = async () => {
+      if (appoid === undefined) {
+        return;
+      }
+      const response = await apiClient.calendar.get({query: { appoid: appoid as string } });
+      const nowEvent = response.body;
+      console.log('nowEvent',nowEvent)
+      setNowEvent(nowEvent); // update the type of nowEvent to match CalendarModel
+  
+      const baseURL = "BEGIN:VCALENDAR\nVERSION:2.0\n";
+      const event = `BEGIN:VEVENT\nSUMMARY:${nowEvent?.title}\nDTSTART:${nowEvent?.startDate}T${nowEvent?.startTime}+0900\nDTEND:${nowEvent?.endDate}T${nowEvent?.endTime}+0900\nDESCRIPTION:${nowEvent?.details}\nLOCATION:${nowEvent?.location}\nEND:VEVENT\n`;
+      const endCalendar = "END:VCALENDAR";
+  
+      const iCalendarData = baseURL + event + endCalendar;
+      const encodedData = encodeURIComponent(iCalendarData);
+      console.log('encodeEvent',encodedData)
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      if (userAgent.indexOf('iphone') > 0) {
+  
+        window.location.href = `webcal://pocketcalendar.app/${encodedData}`;
+      } else if (userAgent.indexOf('android') > 0) {
+  
+        const downloadLink = document.createElement('a');
+        downloadLink.href = `data:text/calendar;charset=utf-8,${encodedData}`;
+        downloadLink.download = 'event.ics';
+        downloadLink.click();
+      } else if (userAgent.indexOf('windows') > 0) {
+  
+        window.open(`data:text/calendar;charset=utf-8,${encodedData}`);
+      }
     }
-    console.log('calendarApp',calendarApp);
-
-
-
-
-
-
-    // const iCalendarData = decodeURIComponent(encodedData);
-
-    // console.log('aa',iCalendarData);
-    // iCalendarデータを使用してカレンダーアプリに追加するロジック
-    // ここでの例はダウンロードリンクを表示するだけです
-    //ほんとはurlハッシュを作ってそれを使ってダウンロードさせたい
-    // const blob = new Blob([iCalendarData], { type: 'text/calendar;charset=utf-8' });
-    // const url = URL.createObjectURL(blob);
-    // console.log('url',url);
-    // setUrl(url);
-
-    // const a = document.createElement('a');
-    // a.href = iCalendarData;
-    // a.download = 'event.ics';
-    // a.textContent = 'Download Event';
-    // document.body.appendChild(a);
-    // setData(iCalendarData); // dataを設定
+      fetchEvent();
   }, []);
 
   if (!user) return <Loading visible />;
