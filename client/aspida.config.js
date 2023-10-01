cb58f451f8bf119d26563b1e1de537fbd9e50e57
s1f102101615 <s1f102101615@iniad.org>
2023-10-02 00:13:31 +0900

    test

--- client/aspida.config.js
+++ client/aspida.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../server/.env' });
+// require('dotenv').config({ path: '../server/.env' });
 
 module.exports = {
   baseURL: `https://antenna-ipxq.onrender.com/`,
