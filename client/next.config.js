56f9aa1ef6946df0127d685edb81509e2dc161a2
s1f102101615 <s1f102101615@iniad.org>
2023-10-05 17:07:11 +0900

    deploy

--- client/next.config.js
+++ client/next.config.js
@@ -6,9 +6,16 @@ module.exports = {
     process.env.GITHUB_REPOSITORY !== undefined
       ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
       : '',
-  output: 'export',
+  output: 'server',
   trailingSlash: true,
   transpilePackages: ['api', 'commonConstantsWithClient'],
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async redirects() {
+    return [
+      // リダイレクトの設定
+      { source: '/event/:appoid', destination: '/event/[appoid]', permanent: true },
+      // 他のリダイレクトルールを追加
+    ];
+  },
 };
